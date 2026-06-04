b58a3d6ca3fa1c6aafd710723ba0da4ffe54fc10
Xileff <felixsavero78@gmail.com>
2026-06-04 18:14:08 +0700

    feat: t.Error() and t.Fatal()

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,26 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T) {
-	result := HelloWorld("felix")
+func TestHelloWorldFelix(t *testing.T) {
+	result := HelloWorld("felixx")
 
 	if result != "Hello world felix" {
-		panic("Result is not Hello world felix")
+		t.Error("Result must be Hello world felix")
 	}
+
+	fmt.Println("TestHelloWorldFelix done")
+}
+
+func TestHelloWorldXilef(t *testing.T) {
+	result := HelloWorld("xileff")
+
+	if result != "Hello world xilef" {
+		t.Fatal("Hello world xilef")
+	}
+
+	fmt.Println("TestHelloWorldXilef done")
 }
