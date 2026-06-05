47dca8ffa4e5ddbf79ed7146e58ab79a8a888eed
Xileff <felixsavero78@gmail.com>
2026-06-05 13:09:57 +0700

    feat: add TestSubTest for HelloWorld function with subtests

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -16,6 +16,22 @@ func TestMain(m *testing.M) {
 	fmt.Println("After unit tests")
 }
 
+func TestSubTest(t *testing.T) {
+	fmt.Println("Start TestSubTest")
+
+	t.Run("Felix", func(t *testing.T) {
+		result := HelloWorld("felix")
+		require.Equal(t, "Hello world felix", result, "Result must be Hello world felix")
+	})
+
+	t.Run("Xilef", func(t *testing.T) {
+		result := HelloWorld("xilef")
+		require.Equal(t, "Hello world xilef", result, "Result must be Hello world xilef")
+	})
+
+	fmt.Println("End TestSubTest")
+}
+
 func TestHelloWorldRequire(t *testing.T) {
 	result := HelloWorld("felix")
 
